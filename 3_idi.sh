b1e3ea3d4229b5010cdd15dbd8cd0d353c11f7fe
Ubuntu <ubuntu@ip-172-31-32-52.us-east-2.compute.internal>
2026-01-21 04:48:04 +0000

    added 3 idi

---
+++ 3_idi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+hero="rancho"
+
+villian="virus"
+
+echo " hero bolo $hero" 
+
+echo "villia $villian"
+#user
+echo "current user $USER"
+
+read -p "whats your dream?" goal
+
+echo "goal is $goal before grad crack a ful time job in my field above 140k"
+
+#arguments
+
+
+echo "movie name: $0"
+
+echo "first idiot: $1"
+
+echo "second: $2"
+
+echo "third: $3"
+
+echo "all 3 idiots: $@"
